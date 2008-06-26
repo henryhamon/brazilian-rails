ed39744b6697a6cc099d679121708a3907eaa7bf
tinogomes <tinorj@gmail.com>
2008-06-26 14:36:07 -0300

    Nova versão de inflections

--- brtraducao/lib/brtraducao/inflector_portuguese.rb
+++ brtraducao/lib/brtraducao/inflector_portuguese.rb
@@ -1,63 +1,60 @@
+# Infelizmente não é possível colocar todas as regras...
+#
+# http://pt.wikipedia.org/wiki/Plural e
+# http://pt.wikipedia.org/wiki/Singular
 module Inflector
   Inflector.inflections do |inflect|
-    inflect.singular(/^(.*)s$/i, '\1')
-    inflect.singular(/^(.*)s$/i, '\1')
-    inflect.singular(/^(.*)s$/i, '\1')
-    inflect.singular(/^(.*)s$/i, '\1')
-    inflect.singular(/^(.*)s$/i, '\1')
- 
-    inflect.plural(/^(.*[z|r])$/i, '\1es')
-    inflect.singular(/^(.*[r|z])es$/i, '\1')
+    inflect.clear
     
-    inflect.plural(/^(.*)al$/i, '\1ais')
-    inflect.plural(/^(.*)el$/i, '\1eis')
-    inflect.plural(/^(.*)ol$/i, '\1ois')
-    inflect.plural(/^(.*)ul$/i, '\1uis')
-    inflect.singular(/^(.*)ais$/i, '\1al')
-    inflect.singular(/^(.*)eis$/i, '\1el')
-    inflect.singular(/^(.*)ois$/i, '\1ol')
-    inflect.singular(/^(.*)uis$/i, '\1ul')
- 
-    inflect.plural(/^(.*)il$/i, '\1is')
+    inflect.plural(/$/,  's')
+    inflect.plural(/(s)$/i,  '\1')
+    inflect.plural(/^(paí)s$/i, '\1ses')
+    inflect.plural(/(z|r)$/i, '\1es')
+    inflect.plural(/al$/i,  'ais')
+    inflect.plural(/el$/i,  'eis')
+    inflect.plural(/ol$/i,  'ois')
+    inflect.plural(/ul$/i,  'uis')
+    inflect.plural(/([^aeou])il$/i,  '\1is')
+    inflect.plural(/m$/i,   'ns')
+    inflect.plural(/^(japon|escoc|ingl|dinamarqu|fregu|portugu)ês$/i,  '\1eses')
+    inflect.plural(/^(|g)ás$/i,  '\1ases')
+    inflect.plural(/ão$/i,  'ões')
+    inflect.plural(/^(irm|m)ão$/i,  '\1ãos')
+    inflect.plural(/^(alem|c|p)ão$/i,  '\1ães')
     
-    inflect.plural(/^(.*)m$/i, '\1ns')
-    inflect.singular(/^(.*)ns$/i, '\1m')
- 
-    inflect.plural(/^(.*s)$/i, '\1es')
-    inflect.plural(/^(.*)ás$/i, '\1ases')
-    inflect.plural(/^(.*)ês$/i, '\1eses')
-    inflect.singular(/^(.*)sses$/i, '\1sse')
+    # Sem acentos...
+    inflect.plural(/ao$/i,  'oes')
+    inflect.plural(/^(irm|m)ao$/i,  '\1aos')
+    inflect.plural(/^(alem|c|p)ao$/i,  '\1aes')
+    
+    inflect.singular(/([^ê])s$/i, '\1')
+    inflect.singular(/^(á|gá|paí)s$/i, '\1s')
+    inflect.singular(/(r|z)es$/i, '\1')
+    inflect.singular(/([^p])ais$/i, '\1al')
+    inflect.singular(/eis$/i, 'el')
+    inflect.singular(/ois$/i, 'ol')
+    inflect.singular(/uis$/i, 'ul')
+    inflect.singular(/(r|t)is$/i, '\1il')
+    inflect.singular(/ns$/i, 'm')
+    inflect.singular(/sses$/i, 'sse')
     inflect.singular(/^(.*[^s]s)es$/i, '\1')
-    
-    inflect.plural(/^(.*)ão$/i, '\1ões')
-    inflect.singular(/^(.*)ões$/i, '\1ão')
- 
-    inflect.plural(/^(.*)ao$/i, '\1oes')
-    inflect.singular(/^(.*)aos$/i, '\1ao')
-    
-    inflect.singular(/^(.*)ães$/i, '\1ão')
-    inflect.singular(/^(.*)ãos$/i, '\1ão')    
-    inflect.singular(/^(.*)oes$/i, '\1ao')
-    inflect.singular(/^(.*)aes$/i, '\1ao')
+    inflect.singular(/ães$/i, 'ão')
+    inflect.singular(/aes$/i, 'ao')
+    inflect.singular(/ãos$/i, 'ão')    
+    inflect.singular(/aos$/i, 'ao')
+    inflect.singular(/ões$/i, 'ão')
+    inflect.singular(/oes$/i, 'ao')
+    inflect.singular(/(japon|escoc|ingl|dinamarqu|fregu|portugu)eses$/i,  '\1ês')
+    inflect.singular(/^(g|)ases$/i,  '\1ás')
     
     # #irregulares
-    irregulares = {   "cão" => 'cães',
-                      "pão" => 'pães',
-                      "mão" => 'mãos',
-                      "alemão" => 'alemães',
-                      "cao" => 'caes',
-                      "pao" => 'paes',
-                      "mao" => 'maos',
-                      "alemao" => 'alemaes',
-                      "mail" => 'mails',
-                      "email" => 'emails'
-                      }
-
-
-    irregulares.each do |key, value|
-      inflect.plural(/^#{key.to_s}$/, value)
-      inflect.singular(/^#{value}$/, key.to_s)
-    end
+#    irregulares = {'país' => 'paises'}
+#
+#
+#    irregulares.each do |key, value|
+#      inflect.plural(/^#{key.to_s}$/, value)
+#      inflect.singular(/^#{value}$/, key.to_s)
+#    end
     
     inflect.uncountable %w( tórax tênis ônibus lápis fênix )
   end
