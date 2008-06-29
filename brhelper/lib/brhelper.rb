658f02f04a52bb7b02b1dde21177e55a20c9ee65
tinogomes <tinorj@gmail.com>
2008-06-28 23:51:20 -0300

    NOVO: Helper para selecao de sexo

--- brhelper/lib/brhelper.rb
+++ brhelper/lib/brhelper.rb
@@ -9,7 +9,8 @@ require 'action_view'
 
   
 %w(version
-br_form_options_helper).each {|req| require File.dirname(__FILE__) + "/brhelper/#{req}"}
+br_form_options_helper
+br_form_helper).each {|req| require File.dirname(__FILE__) + "/brhelper/#{req}"}
 
 
 module BrHelper
--- brhelper/lib/brhelper/br_form_options_helper.rb
+++ brhelper/lib/brhelper/br_form_options_helper.rb
@@ -28,14 +28,13 @@ module ActionView::Helpers::FormOptionsHelper
     ["Tocantins", "TO"]
   ] unless const_defined?("ESTADOS_BRASILEIROS")
   
-  # Helper para montar um select para seleção de estados brasileiros, usando 
-  # option_estados_for_select para gerar a lista de opções.
+  # Helper para montar um select para seleção de estados brasileiros por nome,
+  # mas com armazenamento da sigla.
   def select_estado(object, method, options = {}, html_options = {})
     select object, method, ESTADOS_BRASILEIROS, options, html_options
   end
   
-  # Helper para montar um select para seleção de estados brasileiros, usando 
-  # option_uf_for_select para gerar a lista de opções.
+  # Helper para montar um select para seleção de estados brasileiros por sigla.
   def select_uf(object, method, options = {}, html_options = {})
      select object, method, ESTADOS_BRASILEIROS.collect {|estado, sigla| sigla}, options, html_options
   end
@@ -43,16 +42,18 @@ module ActionView::Helpers::FormOptionsHelper
   # Retorna uma string com a lista de estados brasileiros para usar em uma tag select,
   # com exibição do nome do estado, mas armazenando a sigla.
   def option_estados_for_select
-    html = ""
-    html += options_for_select ESTADOS_BRASILEIROS
-    html
+    options_for_select ESTADOS_BRASILEIROS
   end
   
   # Retorna uma string com a lista de estados brasileiros para usar em uma tag select,
   # com exibição e armazenamento a sigla.
   def option_uf_for_select
-    html = ""
-    html += options_for_select ESTADOS_BRASILEIROS.collect {|nome,sigla| sigla}
-    html
+    options_for_select ESTADOS_BRASILEIROS.collect {|nome,sigla| sigla}
+  end
+  
+  # Helper para montar um select para seleção de sexo, armazenando apenas a 
+  # inicial.
+  def select_sexo(object, method, options = {}, html_options = {})
+    select object, method, [['Masculino', 'M'], ['Feminino', 'F']], options, html_options
   end
 end
